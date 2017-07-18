3ba3e189597ef2e73864e821cf8aae714b158f45
Yow-Long Lee <yowlonglee@gmail.com>
2017-07-18 18:26:31 +0800

    Add more form controls

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 $(document).ready(function() {
 
+  $('.datepicker').kendoDatePicker();
+
+  $('#person').kendoComboBox({
+    dataTextField: "text",
+    dataValueField: "number",
+    dataSource: [
+      {text: '孫小美', number: 1},
+      {text: '阿土伯', number: 2},
+      {text: '錢夫人', number: 3},
+      {text: '李逍遙', number: 4},
+      {text: '林月如', number: 5}
+    ],
+    select: function(event){
+      console.log(event);
+      var number = event.dataItem.number;
+      var sender = event.sender;
+      var display = sender.element.closest('.pic-form-group').find('.combobox-display');
+      display.val(number);
+    }
+  });
+
   // A global variable on opend tabs
   var tabsOpened = ['aa', 'ab', 'ca'];
 
@@ -244,7 +265,7 @@ query_mode();
             if (confirmed) {
               open_message('刪除資料成功','提示');
             }
-          })          
+          })
         }
   	  });
 
